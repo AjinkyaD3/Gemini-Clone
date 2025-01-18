920554da652258c879d7142f9c22758b810fa6e3
Ajinkya D <ajinkyadhotre201@gmail.com>
2025-01-18 15:33:54 +0000

    adding login signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Main from "./components/main/Main"
-import Sidebar from "./components/sidebar/Sidebar"
+import { Route, Routes } from "react-router-dom";
+import Error from "./components/login/Error";
+import Login from "./components/login/Login";
+import Signup from "./components/login/Signup";
+import Main from "./components/main/Main";
+import Sidebar from "./components/sidebar/Sidebar";
 
 const App = () => {
   return (
-    <><Sidebar/>
-    <Main/>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      <Sidebar />
+      <Main />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
